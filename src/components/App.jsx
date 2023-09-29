73caf9fa173768462421b208e0674c8ed95a2a1b
Tymur <tymursheikh4576@gmail.com>
2023-09-29 18:37:23 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,96 +1,73 @@
-import { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
-import contactsTemplate from '../data/contactsTemplate.json';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 import ContactForm from './ContactForm/ContactForm';
 import MainTitle from './MainTitle/MainTitle';
 
-const CONTACTS = JSON.parse(localStorage.getItem('CONTACTS'));
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
 
-export const App = () => {
-  const [contacts, setContacts] = useState(CONTACTS ?? []);
-  const [filter, setFilter] = useState('');
-  const [isInitializedTemplate, setIsInitializedTemplate] = useState(
-    CONTACTS && CONTACTS.length > 0 ? true : false
-  );
-  const [timer, setTimer] = useState(3);
-
-  useEffect(() => {
-    if (!isInitializedTemplate) {
-      setTimeout(() => {
-        setContacts(contactsTemplate);
-        setIsInitializedTemplate(true);
-      }, 3000);
-    }
-  }, [isInitializedTemplate]);
-
-  useEffect(() => {
-    if (timer > 0) {
-      const interval = setInterval(() => {
-        setTimer(prevTimer => prevTimer - 1);
-      }, 1000);
-
-      return () => clearInterval(interval);
-    }
-  }, [timer]);
-
-  useEffect(() => {
-    window.localStorage.setItem('CONTACTS', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const addNewContacts = (name, number) => {
+  addNewContacts = (name, number) => {
     const showAlert = true;
     const similarElement = element => element.name === name;
-    if (contacts.find(similarElement)) {
+    if (
+
+      this.state.contacts.some(similarElement)
+    ) {
       alert(name + ' is already in contacts.');
       return showAlert;
     }
 
-    setContacts(prevState => [{ id: nanoid(), name, number }, ...prevState]);
+    this.setState(prevState => ({
+      contacts: [{ id: nanoid(), name, number }, ...prevState.contacts],
+    }));
   };
 
-  const handlerInputFilter = e => {
+  handlerInputFilter = e => {
     const { value } = e.target;
-    setFilter(value);
+    this.setState({ filter: value });
   };
 
-  const handlerButtonDelete = id => {
-    setContacts(prevState => prevState.filter(contact => contact.id !== id));
+  handlerButtonDelete = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
   };
 
-  const filterContacts = () => {
+  filterContacts = () => {
+    const { contacts, filter } = this.state;
     const lowerCaseFilter = filter.toLowerCase();
+
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(lowerCaseFilter)
     );
   };
 
-  return (
-    <div className="container">
-      <MainTitle title="Phonebook" />
-      <ContactForm addNewContacts={addNewContacts} />
-      <MainTitle title="Contacts" />
-      <Filter onChange={handlerInputFilter} filterValue={filter} />
-
-      {isInitializedTemplate ? (
-        <ContactList
-          contacts={filterContacts()}
-          onButtonDelete={handlerButtonDelete}
+  render() {
+    const filterContacts = this.filterContacts();
+    return (
+      <div className="container">
+        <MainTitle title="Phonebook" />
+        <ContactForm addNewContacts={this.addNewContacts} />
+        <MainTitle title="Contacts" />
+        <Filter
+          onChange={this.handlerInputFilter}
+          filterValue={this.state.filter}
         />
-      ) : (
-        <>
-          <p>
-            You don't have any saved contacts, templates for contacts will be
-            loaded
-          </p>
-          <p>
-            Ви не маєте збережених контактів, будуть завантажені шаблони
-            контактів
-          </p>
-          <p>{timer}</p>
-        </>
-      )}
-    </div>
-  );
-};
+        <ContactList
+          contacts={filterContacts}
+          onButtonDelete={this.handlerButtonDelete}
+        />
+      </div>
+    );
+  }
+}
